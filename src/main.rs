12655eb5ee92eacfd9ec43428915e356ec4e7e1a
hisomura <hisomura.noreply@gmail.com>
2020-10-20 20:08:10 +0900

    文字列を試してみた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let s1: String = String::from("Hello, World");
+    let s2: &str = &s1;
+    let s3: String = s2.to_string();
+
+    println!("{}", s1);
+    println!("{}", s2);
+    println!("{}", s3);
+
+
+    let mut s4: String = String::from("Hello, World");
+    // let s5: &str = &s4;
+    s4.make_ascii_lowercase();
+    println!("{}", s4);
 }
