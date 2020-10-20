c5d65b9a129a0addc0dbacbe466c0d73baeb1544
hisomura <hisomura.noreply@gmail.com>
2020-10-21 07:41:35 +0900

    配列の動作を確認

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 fn main() {
-    let s1: String = String::from("Hello, World");
-    let s2: &str = &s1;
-    let s3: String = s2.to_string();
-
-    println!("{}", s1);
-    println!("{}", s2);
-    println!("{}", s3);
-
-
-    let mut s4: String = String::from("Hello, World");
-    // let s5: &str = &s4;
-    s4.make_ascii_lowercase();
-    println!("{}", s4);
+    let mut a: [i32; 3] = [0, 1, 2];
+    let b: [i32; 3] = [0; 3];
+    println!("{:?}", a);
+    a[1] =b[1];
+    a[2] =b[2];
+    // println!("{:?}", a[1..3]); // error
+    println!("{:?}", &a[1..3]);
+    println!("{:?}", a);
 }
